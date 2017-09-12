c6d3066dd8239b631990ccb00b5308bf3d1546db
V3964 (Conference ID) <v3964@pcgeogtl11.geog.pwf.cam.ac.uk>
2017-09-12 14:41:39 +0100

    First commit

---
+++ numerics/Burger_equation.py
@@ -0,0 +1,68 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+def initialBell(x): 
+    """Intitial Funtion"""
+    print(initialBell.__doc__)
+    return np.where(x%1.<0.5, np.power(np.sin(2*x*np.pi), 2), 0) 
+    # x%1 = x mod 1. 
+
+def periodic_BC(phi,c,nx):
+    phi_start = phi[0] - 0.5*c[0]*(phi[1] - phi[nx-1])
+    print('phi[0] is ',phi_start)
+    return phi_start
+    
+def first_timestep(phi_old,c,nx):
+    """ FTCS for the first timestep """
+    phi = phi_old.copy()
+    for j in xrange(1,nx):
+        phi[j] = phi_old[j] - (c[j]/2.)*(phi_old[j+1] - phi_old[j-1])
+        # use c in each spatial position j
+    return phi
+    
+    
+def Burger(phi_old,phi,c,nx):
+    """ Non-linear advection, CTCS scheme """
+    phi_new=phi_old.copy()
+    for j in xrange(1,nx):
+        phi_new[j] = phi_old[j] - c[j]*(phi[j+1] - phi[j-1])       
+    return phi_new
+    
+def main():
+    nx = 100
+    nt = 30
+    dt = 0.005
+    dx = 1./nx
+    
+    x=np.linspace(0.0, 1.0, nx+1)
+    phi = initialBell(x)
+    phi_old = phi.copy()
+    
+    c = phi*dt/dx    # for boundary condition calculation    
+    phi[0] = periodic_BC(phi,c,nx)
+    phi[nx] = phi[0]
+    
+
+    phi = first_timestep(phi_old,c,nx)
+    
+    c=np.zeros((nx+1,nt+1))
+
+    
+    for i in xrange(1,nt):
+        c[:,i] = phi*dt/dx # c is different for each time step
+        print(np.max(c[:,i]))
+        phi_new = Burger(phi_old,phi,c[:,i],nx)
+        phi_old = phi.copy()
+        phi = phi_new.copy()
+    
+    fig = plt.figure()
+    plt.plot(x,initialBell(x),'b',label='initial')
+    plt.plot(x,phi,'r',label='final')
+    plt.annotate('c_max=%.2f'%(np.max(c)),xy=(0.15,0.8),xycoords='figure fraction')
+    plt.legend()
+    plt.show()
+    return c, phi 
+if __name__ == '__main__':
+    c, phi = main()
+    
+    
